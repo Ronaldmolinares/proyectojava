5af27f734e2709e8d82a3698e78b1ba72c4aae69
Samir <ronald.molinares01@uptc.edu.co>
2022-06-29 09:55:43 -0500

    mi primer commit

---
+++ src/clase/taller.java
@@ -0,0 +1,33 @@
+package clase;
+
+public class taller { 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a = 1;
+ double b = 2.5;
+ double z;
+ z = (double) a + b;
+		
+ int suma;
+ suma= a + a;
+ 
+System.out.println(suma);
+System.out.println(z);
+
+ String s = "hola";
+ int l = 5;
+ String g = s + l ;
+ System.out.println(g);
+ 
+ String data;
+ data = s + l;
+ 
+ System.out.println(data);
+ char c = 'ñ';
+ System.out.println(data + " " +l + " " + c);
+ 
+ 
+	}
+
+}
